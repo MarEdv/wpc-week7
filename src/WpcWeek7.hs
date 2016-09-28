209edd0d023d19c78afec8deae494f73f63a3f71
Marcus Edvinsson <edvinsson@gmail.com>
2016-09-28 21:43:25 +0200

    More of everything - Documentation - Unit tests - Bugfixes

--- src/WpcWeek7.hs
+++ src/WpcWeek7.hs
@@ -1,67 +1,154 @@
-module WpcWeek7(Tree (..),searchNode,insert,link) where
+module WpcWeek7(BplusTree (..),searchNode,insert,link,findChild,insert2,getLowestKey,flatmap,createNode) where
 import Data.Maybe
 import Data.List(sortOn)
 
-data Tree key value =
-  Node Int [key] [Tree key value] (Maybe (Tree key value))
-  |
-  Leaf Int [(key, value)] (Maybe (Tree key value))
-  deriving (Show, Eq)
 
-searchNode :: (Eq key) => key -> Tree key value -> Maybe (Tree key value)
+{-
+Data structure for a B+Tree according to the algorithm described at https://en.wikipedia.org/wiki/B%2B_tree
+
+A BplusTree is parameterized with two types, one for keys and one for values.
+
+A Node is a non-leaf node in the tree
+ - arity is the maximum number of keys per node
+ - keys is a list of the lowest key of each child (except the first child)
+ - children is a list of children, either of type Node or Leaf. The list of children always contains one more element than the list of keys.
+ - sibling is "the next Node node"
+A Leaf is a leaf node in the tree
+ - arity is the maximum number of keys per node
+ - keyValues is a list of tuples of key and value
+ - sibling is "the next Leaf node"
+-}
+data BplusTree key value =
+    Node {
+      arity :: Int
+      , keys :: [key]
+      , children :: [BplusTree key value]
+      , sibling :: (Maybe (BplusTree key value))
+    }
+  |
+    Leaf {
+       arity :: Int
+       , keyValues :: [(key, value)]
+       , sibling :: (Maybe (BplusTree key value))
+    }
+  deriving (Eq)
+
+showBplusTree :: (Show key, Show value) => BplusTree key value -> ShowS
+showBplusTree (Leaf arity' keyValues' _) = shows ("Leafs " ++ (show arity') ++ " " ++ (show keyValues'))
+showBplusTree (Node arity' keys' children' _) = shows ("Node " ++ (show arity') ++ " " ++ (show keys') ++ " " ++ (show children'))
+
+instance (Show key, Show value) => Show (BplusTree key value) where
+  showsPrec _ x = showBplusTree x
+
+{-
+Maybe finds a Leaf node that contains a certain key.
+-}
+searchNode :: (Ord key) => key -> BplusTree key value -> Maybe (BplusTree key value)
 searchNode _ t@(Leaf _ _ _) = Just t
-searchNode k (Node _ keys children _) = x where
-  y = findChild k keys children
+searchNode k (Node _ keys' children' _) = x where
+  y = findChild k keys' children'
   x = case y of
     Just c -> searchNode k c
     Nothing -> Nothing
 
-findChild :: (Eq a) => a -> [a] -> [b] -> Maybe b
-findChild key (k:[]) (c:cs)
-  | key == k = Just c
-  | otherwise = Just (head cs)
+{-
+A utility function of a flatmap
+-}
+flatmap :: (a -> [b]) -> [a] -> [b]
+flatmap _ [] = []
+flatmap f xs = (>>=) xs f
+--flatmap f (x:xs) = f x ++ flatmap f xs
+
+{-
+The elements of the list are ascendingly ordered.
+The function locates the first element that is higher than the key.
+The BplusTree node in the same position contains the key.
+-}
+findChild :: (Ord key) => key -> [key] -> [BplusTree key value] -> Maybe (BplusTree key value)
+findChild _ [] (c:[]) = Just c
 findChild key (k:ks) (c:cs)
-  | key == k = Just c
+  | key < k = Just c
   | otherwise = findChild key ks cs
 findChild _ _ _ = Nothing
 
-flatmap _ [] = []
-flatmap f (x:xs) = f x ++ flatmap f xs
+{-
+Inserts a key mapping to a value into a BplusTree. The function returns the new root
+node of a new tree in line with other persistent data structures.
+-}
+insert :: (Ord key, Eq value) => key -> value -> BplusTree key value -> BplusTree key value
+insert key' value' root@(Node arity' _ _ _) = x where
+  ys = insert2 key' value' root
+  x = if (length ys == 1)
+      then head ys
+      else createNode arity' Nothing ys
 
-findChild2 :: (Ord a) => a -> [a] -> [Tree key value] -> Tree key value
-findChild2 _ [] (c:[]) = c
-findChild2 key (k:ks) (c:cs)
-  | key < k = c
-  | otherwise = findChild2 key ks cs
+insert _ _ root = root
 
-insert :: (Ord key, Eq value) => key -> value -> Tree key value -> Tree key value
-insert k v n = head $ insert2 k v n
+{-
+A helper function that inserts a key-value pair and returns a list of tree nodes.
+The function returns two elements in the list when a node has been split into
+two nodes, to be able to honor the tree's arity.
+-}
+insert2 :: (Ord key, Eq value) => key -> value -> BplusTree key value -> [BplusTree key value]
+insert2 key' value' (Node arity' keys' children' _) = x where
+  Just child = findChild key' keys' children'
+  newChildren = flatmap (\c -> if (child == c)
+                     then (insert2 key' value' c)
+                     else [c]
+              )
+              children'
+  (n1, n2) = perhapsSplitArray arity' newChildren
+  newNode = if (length n2 > 0)
+            then [createNode arity' Nothing n2]
+            else []
+  x = [createNode arity' Nothing n1] ++ newNode
+insert2 key' value' (Leaf arity' keyValues' _) = x where
+  kvsExceptNewKey = filter (\(d,_) -> d /= key') keyValues'
+  kvsWithNewKey = kvsExceptNewKey ++ [(key',value')]
+  sortedKeyValues = sortOn (\(a,_) -> a) kvsWithNewKey
+  (kv1, kv2) = perhapsSplitArray arity' sortedKeyValues
+  newLeaf = if (length sortedKeyValues < arity')
+            then []
+            else [Leaf arity' kv2 Nothing]
+  x = [Leaf arity' kv1 Nothing] ++ newLeaf
 
-insert2 :: (Ord key, Eq value) => key -> value -> Tree key value -> [Tree key value]
-insert2 k v n@(Node arity keys children sibling) = x where
-  a = findChild2 k keys children
-  y = flatmap (\c -> if (a == c) then (insert2 k v c) else [c]) children
-  z = map (\c -> case c of
-              Leaf _ kv _ -> fst (head kv)
-              Node _ ks _ _ -> head $ ks
-          ) y
-  x = [Node arity (tail z) (link y) sibling]
-insert2 k v l@(Leaf arity keyValues sibling) = x where
-  z = keyValues ++ [(k,v)]
-  y = sortOn (\(a,_) -> a) z
-  halfArity = arity `div` 2 + 1
-  (kv1, kv2) = if (length y < arity)
-               then (y,[])
-               else (take halfArity y, drop halfArity y)
-  c1 = if (length y < 3)
-       then []
-       else [Leaf arity kv2 Nothing]
-  x = [Leaf arity kv1 Nothing] ++ c1
+createNode :: (Ord key) => Int -> Maybe (BplusTree key value) -> [BplusTree key value] -> BplusTree key value
+createNode arity' sibling' children' = node where
+  childIndex = flatmap getLowestKey children'
+  newChildIndex = if (length childIndex > 0)
+                  then tail childIndex
+                  else []
+  linkedChildren = link children'
+  node = Node arity' newChildIndex linkedChildren sibling'
 
-link :: (Eq keys) => [Tree keys values] -> [Tree keys values]
+{-
+Gets the lowest key of the Leaf or of the Leaf reachable from a Node.
+-}
+getLowestKey :: BplusTree key value -> [key]
+getLowestKey c = case c of
+  Leaf _ kv _ -> [fst (head kv)]
+  Node _ _ cs _ -> getLowestKey (head cs)
+
+{-
+If the element count in the array is equal to larger than the numeric argument,
+the array is split into two as equally size arrays as possible.
+  -}
+perhapsSplitArray :: Int -> [a] -> ([a], [a])
+perhapsSplitArray n xs = (kv1, kv2) where
+  halfN = n `div` 2 + 1
+  (kv1, kv2) = if (length xs < n)
+               then (xs,[])
+               else (take halfN xs, drop halfN xs)
+
+{-
+Updates the sibling field of all Leaf or Node objects in the list
+passed as first argument. Each element will reference to the next in the list
+as its sibling.
+-}
+link :: (Eq keys) => [BplusTree keys values] -> [BplusTree keys values]
 link [] = []
 link (x:xs) = z where
   y = link $ xs
   z = case x of
-    Leaf a keys k -> [(Leaf a keys (listToMaybe y))] ++ y
-    Node _ _ _ _ -> []
+    Leaf arity' keys' _ -> [(Leaf arity' keys' (listToMaybe y))] ++ y
+    Node arity' keys' children' _ -> [(Node arity' keys' children' (listToMaybe y))] ++ y
--- test/WpcWeek7/Test.hs
+++ test/WpcWeek7/Test.hs
@@ -1,62 +1,95 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module WpcWeek7.Test
-    (bplusSuite)
+    (bplusSuite,bulkInsert,containsAll)
 where
 
 import Test.Tasty (testGroup, TestTree)
 import Test.Tasty.HUnit
---import Test.Tasty.SmallCheck as SC
+import Test.Tasty.SmallCheck as SC
+import Data.List (nub)
 
 import WpcWeek7
-
--- Make instance of CoconutDataStruct
--- we simply use consN Constr where N is the arity of Constr (SmallCheck)
--- we also needed the
--- {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
---import Test.SmallCheck.Series
-
---instance Monad m => Serial m CoconutDataStruct  where series = cons1 CoconutConstr
--- Now we could test properties with smallcheck on CoconutDataStruct type.
 
 bplusSuite :: TestTree
 bplusSuite = testGroup "bplus"
     [ testCase "bplus" testInsert
     , testCase "searchNode" testSearchNode
     , testCase "insert" testInsert2
-    , testCase "link" testLink
---    , SC.testProperty "bplus property" prop_bplus
+    , testCase "linkLeafs" testLinkLeafs
+    , testCase "linkNodes" testLinkNodes
+    , testCase "maxArity" testMaxArity
+    , SC.testProperty "insertedElementIsFound" insertedElementIsFound
+    , SC.testProperty "headHasCorrectNoChildren" headHasCorrectNoChildren
+    , SC.testProperty "headHasCorrectNoChildren2" headHasCorrectNoChildren2
     ]
 
 testInsert :: Assertion
-testInsert = assertEqual "Stuff" (Leaf 3 [(1,'a')] Nothing) (Leaf 3 [(1,'a')] Nothing)
+testInsert = assertEqual "Stuff" ((Leaf 3 [(1,'a')] Nothing) :: (BplusTree Int Char)) (Leaf 3 [(1,'a')] Nothing)
 
-bnode1 :: Tree Int Char
+bnode1 :: BplusTree Int Char
 bnode1 = Leaf 3 [(6,'f'),(7,'g')] Nothing
 
-btree1 :: Tree Int Char
+btree1 :: BplusTree Int Char
 btree1 = x where
   y3 = Leaf 3 [(6,'f'),(7,'g')] Nothing
   y2 = Leaf 3 [(3,'c'),(4,'d')] (Just y3)
   y1 = Leaf 3 [(1,'a'),(2,'b')] (Just y2)
   x = Node 3 [3,6] [y1, y2, y3] Nothing
 
-btree2 :: Tree Int Char
+btree2 :: BplusTree Int Char
 btree2 = x where
   y4 = Leaf 3 [(6,'f'),(7,'g')] Nothing
   y3 = Leaf 3 [(5,'d')] (Just y4)
-  y2 = Leaf 3 [(3,'c'),(4,'d')] (Just y3)
+  y2 = Leaf 3 [(3,'c'),(4,'d')] Nothing
   y1 = Leaf 3 [(1,'a'),(2,'b')] (Just y2)
-  x = Node 3 [3,5,6] [y1, y2, y3, y4] Nothing
-  
+  z2 = Node 3 [6] [y3, y4] Nothing
+  z1 = Node 3 [3] [y1, y2] (Just z2)
+  x = Node 3 [5] [z1, z2] Nothing
+
 testSearchNode :: Assertion
 testSearchNode = assertEqual "yo" (Just bnode1) (searchNode 7 btree1)
 
 testInsert2 :: Assertion
 testInsert2 = assertEqual "yo2" btree2 (insert 5 'd' btree1)
 
-testLink :: Assertion
-testLink = assertEqual "link" [(Leaf 3 [(1,'a')] (Just (Leaf 3 [(2,'b')] Nothing))),(Leaf 3 [(2,'b')] Nothing)] (link [(Leaf 3 [(1,'a')] Nothing), (Leaf 3 [(2,'b')] Nothing)])
+testLinkLeafs :: Assertion
+testLinkLeafs = assertEqual "link" ([(Leaf 3 [(1,'a')] (Just (Leaf 3 [(2,'b')] Nothing))),(Leaf 3 [(2,'b')] Nothing)] :: [BplusTree Int Char]) (link [(Leaf 3 [(1,'a')] Nothing), (Leaf 3 [(2,'b')] Nothing)])
 
---prop_coconut :: Property IO
---prop_coconut = forAll $ \coconutStruct -> coconutfunc coconutStruct >= 0
+testLinkNodes :: Assertion
+testLinkNodes = assertEqual "link" ([(Node 3 [] [] (Just (Node 3 [] [] Nothing))),(Node 3 [] [] Nothing)] :: [BplusTree Int Char]) (link [(Node 3 [] [] Nothing), (Node 3 [] [] Nothing)])
+
+testMaxArity :: Assertion
+testMaxArity = assertEqual "maxArity" 2 (maxArity $ Just btree1)
+
+maxArity :: Maybe (BplusTree keys values) -> Int
+maxArity Nothing = 0
+maxArity (Just node) = x where
+  y = case node of
+    Leaf _ kv s -> (length kv, maxArity s)
+    Node _ kv _ s -> (length kv, maxArity s)
+  x = max (fst y) (snd y)
+
+insertedElementIsFound :: Property IO
+insertedElementIsFound = forAll $ \list -> containsAll (list::[Int]) (bulkInsert (list))
+
+headHasCorrectNoChildren :: Property IO
+headHasCorrectNoChildren = forAll $ \list -> length (nub list) < 3 || case (bulkInsert (list::[Int])) of
+  Node arity' _ children' _ -> length children' >= 2 && length children' <= arity'
+  _ -> False
+
+headHasCorrectNoChildren2 :: Property IO
+headHasCorrectNoChildren2 = forAll $ \list -> length (nub list) > 3 || case (bulkInsert (list::[Int])) of
+  Node arity' _ children' _ -> length children' >= 1 && length children' < arity'
+  _ -> False
+
+containsAll :: [Int] -> BplusTree Int values -> Bool
+containsAll (x:xs) node = case searchNode x node of
+  Just (Leaf _ keyValues' _) -> (length $ filter (\a -> fst a == x) keyValues') == 1 && containsAll xs node
+  _ -> False
+containsAll [] _ = True
+
+bulkInsert :: [Int] -> BplusTree Int String
+bulkInsert [] = Node 3 [] [Leaf 3 [] Nothing] Nothing
+bulkInsert (x:xs) = insert x (show x) (bulkInsert xs)
+
